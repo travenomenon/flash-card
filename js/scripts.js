7594278d1d3aa029e1ef5b8d3355568ad6061918
Tien Le and Travis Wainwright <tien@>
2019-01-15 15:04:04 -0800

    add functional flip card element

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#flip1").click(function() {
+    $("#def1").toggle();
+    $("#term1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flip2").click(function() {
+    $("#def2").toggle();
+    $("#term2").toggle();
+  });
+});
